96aa47f9561731668a66fe3b9a46f02b688faecc
ABDUL MONNAF <49575110+amrajon@users.noreply.github.com>
2021-04-09 23:26:44 +0200

    Add files via upload

---
+++ App_Code/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_VAMS.App_Code
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+        public User(int id, string username, string password, string role)
+        {
+            Id = id;
+            UserName = username;
+            Password = password;
+            Role = role;
+        }
+
+        public User(string username, string password, string role)
+        {
+            UserName = username;
+            Password = password;
+            Role = role;
+        }
+    }
+}
