967d6a39bc3589277c10f6a909d1891700eb788e
ABDUL MONNAF <49575110+amrajon@users.noreply.github.com>
2021-04-13 22:29:07 +0200

    Add files via upload

---
+++ UnitTest/loginTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class loginTest
+    {
+        [TestMethod]
+        public void AdminPageRedirecting_UserRoleIsAdmin_ReturnTrue()
+        {
+            //Arrange
+            var Login = new Login();
+
+            //Act
+            var Login = Login.AdminPageRedirecting(new User {IsAdministrator = true && Response.Redirect("~/Pages/Management/AdminPage.aspx")= true});
+
+            //Assert
+            Assert.IsTrue(Login);
+        }
+    }
+}
